0395d2812c7365f78c7066e47ec1ebc910c9451f
Johan Garzon <johanprograma@hotmail.com>
2020-02-05 22:55:26 +0100

    Class transfer: Method isSaldoEnough() is working

--- API/classes/Transfer.php
+++ API/classes/Transfer.php
@@ -5,18 +5,40 @@ class Transfer
     private $from_amount;
     private $from_account;
     private $from_currency;
+    private $from_balance;
     private $to_amount;
     private $to_account;
     private $to_currency;
+    private $conn;
 
-    public function checkSaldo()
-    {
-        //Checkar si ammount es mayor que saldo.
+
+    public function __construct(Account $accFrom, Account $accTo, $amount, Database $db) {
+        $this->from_amount = $amount;
+        $this->from_account = $accFrom->getAccountId();
+        $this->from_currency = $accFrom->getCurrency();
+        $this->from_balance = $accFrom->getBalance();
+        $this->to_amount = $amount;
+        $this->to_account = $accTo->getAccountId();
+        $this->to_currency = $accTo->getCurrency();
+        $this->conn = $db;
     }
 
+    /**
+     * Check if Account balance is
+     * not less than the amount.
+     * Because the balance should not be less than 0
+     */
+    public function isSaldoEnough()
+    {
+        return ($this->from_balance >= $this->from_amount);
+    }
+
+    /**
+     * Make the transfer into the database
+     */
     public function makeTransfer()
     {
-        // Hacer transferencia
+        // Hacer transferencia en DB
     }
 
 }
--- API/providers/userAndMore.php
+++ API/providers/userAndMore.php
@@ -3,12 +3,27 @@
 include '../classes/Db.php';
 include '../classes/User.php';
 include '../classes/Account.php';
+include '../classes/Transfer.php';
 header('Content-Type: application/json');
 
 $db = new DataBase();
-$user = new User($db);
-$user->constructUser(9);
-$cuenta = new Account($user);
-echo $cuenta->getBalance();
+$user1 = new User($db);
+$user1->constructUser(9);
+$cuenta1 = new Account($user1);
+$user2 = new User($db);
+$user2->constructUser(3);
+$cuenta2 = new Account($user2);
+echo $cuenta1->getUserId() . PHP_EOL;
+echo $cuenta1->getBalance() . $cuenta1->getCurrency() . PHP_EOL;
+echo $cuenta2->getUserId() . PHP_EOL;
+echo $cuenta2->getBalance() . $cuenta2->getCurrency() . PHP_EOL;
+
+$transferencia = new Transfer($cuenta1, $cuenta2, 50974, $db);
+if ($transferencia->isSaldoEnough()) {
+    echo "Es suficiente";
+} else {
+    echo "No es suficiente";
+}
+
 
 //print_r($user->constructUser(9));
