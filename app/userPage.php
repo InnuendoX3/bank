9876f2aa9198ffe45338f426a40802e183e8ed26
Johan Garzon <johanprograma@hotmail.com>
2020-02-06 11:59:03 +0100

    UserPage.php added Bootstrap

--- app/userPage.php
+++ app/userPage.php
@@ -19,24 +19,29 @@ echo "</pre>"; */
 
 <?php require_once "./partials/head_jquery.php"; ?>
 
-<body>
+<body class="row d-flex justify-content-center text-center">
+    <div class="mt-5">
     <h1 id="name"></h1>
     <a href="/bank/app/login.php?q=logout">Logout</a>
     <h2>Saldo: <span id="saldo"></span></h2>
     <form action="/bank/API/providers/transfer.php" method="post">
-        <label>Select recipient </label>
-        <select name="subUserId" id="usersDropdown">
-            <option value="" disabled selected>Choose an user</option>
-            <!-- Here AJAX gets the users -->
-        </select>
-        <br>
-        <p>Account nr. <span id="account"></span></p>
-        <p>Mobilephone: <span id="phone"></span></p>
-        <label for="amount">Enter amount </label>
-        <input type="number" name="amount" id="">
+        <div class="form-group">
+            <label>Select recipient </label>
+            <select name="subUserId" id="usersDropdown" class="form-control">
+                <option value="" disabled selected>Choose an user</option>
+                <!-- Here AJAX gets the users -->
+            </select>
+        </div>
+        <p> <strong>Account nr. </strong><span id="account">---</span></p>
+        <p> <strong>Mobilephone: </strong><span id="phone">---</span></p>
+        <div class="form-group">
+            <label for="amount">Enter amount </label>
+            <input type="number" name="amount" class="form-control">
+        </div>
         <input type="hidden" name="bigUserId" value="<?php echo $_SESSION['id'] ?>">
-        <input type="submit" id="botoncito" value="transfer" name="send"> 
+        <input type="submit" id="botoncito" value="transfer" name="send" class="form-control btn btn-primary"> 
     </form>
+    </div>
 
     <script>
         // Saves the PHP ID variable to  a JS variable for AJAX function
